58fe02d408f8b6f653fd4acc6e3a4223dea32838
Egor <egor.osokin7@gmail.com>
2020-12-16 21:41:47 +0300

    fix: urls

--- condor/condor/urls.py
+++ condor/condor/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 urlpatterns += [
     path("", include("catalog.urls")),
     path("/", include("catalog.urls")),
-    path("/*", include("catalog.urls")),
+    path("*", include("catalog.urls")),
 ]
 
 
